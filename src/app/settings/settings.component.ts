9bb3baa145d7c214bf6dc15c0d79990474a778fd
Carlos Ortiz <cortizgomez@hotmail.com>
2021-02-03 14:24:59 -0800

    Excercise: use component in settings page.

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'at-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  statistic = {
+    icon : "fa-cloud-download",
+    label : "Days Uptime",
+    value : 20
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
